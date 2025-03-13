203a555fa11c33dc9531335f2cc9d63b870cecec
VronzeWook <chanel0222@naver.com>
2025-03-13 19:45:30 +0900

    section6-SingletonContainer

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -25,4 +25,9 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(discountPrice, itemPrice, itemName, memberId);
     }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
